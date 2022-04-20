623340d678611fe2b532b2550063cc3db46292c2
Shaparapatah <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>
2022-04-20 21:48:05 +0300

    6-е ДЗ.

--- app/src/androidTest/java/com/geekbrains/tests/automator/BehaviorTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/automator/BehaviorTest.kt
@@ -57,7 +57,6 @@ class BehaviorTest {
         val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
         //Проверяем на null
         Assert.assertNotNull(editText)
-        Assert.assertNull(uiDevice.findObject(By.res(packageName, "searchEditText1")))
     }
 
     //Убеждаемся, что поиск работает как ожидается
@@ -71,15 +70,6 @@ class BehaviorTest {
         Espresso.onView(ViewMatchers.withId(R.id.searchEditText))
             .perform(ViewActions.pressImeActionButton())
 
-
-        val toDetails: UiObject2 = uiDevice.findObject(
-            By.res(
-                packageName,
-                "toSearchActivityButton"
-            )
-        )
-        toDetails.click()
-
         //Ожидаем конкретного события: появления текстового поля totalCountTextView.
         //Это будет означать, что сервер вернул ответ с какими-то данными, то есть запрос отработал.
         val changedText =
@@ -90,7 +80,6 @@ class BehaviorTest {
         //Убеждаемся, что сервер вернул корректный результат. Обратите внимание, что количество
         //результатов может варьироваться во времени, потому что количество репозиториев постоянно меняется.
         Assert.assertEquals(changedText.text.toString(), "Number of results: 668")
-        Assert.assertNotEquals(changedText.text.toString(), "Number of results: 663")
     }
 
     //Убеждаемся, что DetailsScreen открывается
@@ -119,122 +108,6 @@ class BehaviorTest {
         //Чтобы проверить отображение определенного количества репозиториев,
         //вам в одном и том же методе нужно отправить запрос на сервер и открыть DetailsScreen.
         Assert.assertEquals(changedText.text, "Number of results: 0")
-        Assert.assertNotEquals(changedText.text, "Number of results: 1")
-    }
-
-    @Test //
-    fun activity_CheckWorkBackButton() {
-
-        val toDetails = uiDevice.findObject(By.res(packageName, "toDetailsActivityButton"))
-        toDetails.click()
-        Assert.assertNotNull(
-            uiDevice
-                .wait(Until.findObject(By.text("Number of results: 0")), TIMEOUT)
-        )
-        Assert.assertNull(
-            uiDevice
-                .wait(Until.findObject(By.text("Number of results: 1")), TIMEOUT)
-        )
-        uiDevice.pressBack()
-        Assert.assertNotNull(uiDevice.findObject(By.text("ПОИСК РЕПОЗИТОРИЕВ")))
-        Assert.assertNull(uiDevice.findObject(By.text("ПОИСК РЕПОЗИТОРИЕВ1")))
-    }
-
-    @Test
-    fun activity_ExistsElementsOnMainActivity() {
-
-        Assert.assertNotNull(uiDevice.findObject(By.text("Enter keyword e.g. android")))
-        Assert.assertNull(uiDevice.findObject(By.text("Enter keyword e.g. android1")))
-        Assert.assertNotNull(uiDevice.findObject(By.text("Search Repositories")))
-        Assert.assertNull(uiDevice.findObject(By.text("Search Repositories1")))
-        Assert.assertNotNull(uiDevice.findObject(By.text("Details callback")))
-        Assert.assertNull(uiDevice.findObject(By.text("Details callback1")))
-    }
-
-    @Test
-    fun activity_ExistsElementsOnDetailsActivity() {
-        val toDetails = uiDevice.wait(
-            Until.findObject(
-                By.res(packageName, "toDetailsActivityButton")
-            ), TIMEOUT
-        )
-        toDetails.click()
-        Assert.assertNotNull(
-            uiDevice.wait(
-                Until.findObject(By.res(packageName, "decrementButton")), TIMEOUT
-            )
-        )
-        Assert.assertNull(
-            uiDevice.wait(
-                Until.findObject(By.res(packageName, "decrementButton1")), TIMEOUT
-            )
-        )
-        Assert.assertNotNull(uiDevice.findObject(By.text("Number of results: 0")))
-        Assert.assertNull(uiDevice.findObject(By.text("Number of results: 1")))
-        Assert.assertNotNull(
-            uiDevice.wait(
-                Until.findObject(By.res(packageName, "incrementButton")), TIMEOUT
-            )
-        )
-        Assert.assertNull(
-            uiDevice.wait(
-                Until.findObject(By.res(packageName, "incrementButton1")), TIMEOUT
-            )
-        )
-    }
-
-    @Test
-    fun activity_DetailsScreenButtonsIsWorking() {
-        val toDetails = uiDevice.wait(
-            Until.findObject(
-                By.res(packageName, "toDetailsActivityButton")
-            ), TIMEOUT
-        )
-        toDetails.click()
-        val decrementButton = uiDevice.wait(
-            Until.findObject(
-                By.res(packageName, "decrementButton")
-            ), TIMEOUT
-        )
-        val incrementButton = uiDevice.wait(
-            Until.findObject(
-                By.res(packageName, "incrementButton")
-            ), TIMEOUT
-        )
-        incrementButton.click()
-        Assert.assertNotNull(uiDevice.findObject(By.text("Number of results: 1")))
-        Assert.assertNull(uiDevice.findObject(By.text("Number of results: 0")))
-        decrementButton.click()
-        Assert.assertNotNull(uiDevice.findObject(By.text("Number of results: 0")))
-        Assert.assertNull(uiDevice.findObject(By.text("Number of results: 1")))
-    }
-
-    @Test
-    fun activity_SuccessInsertText() {
-        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
-        editText.text = "someText"
-        Assert.assertNotNull(uiDevice.findObject(By.text("someText")))
-        Assert.assertNull(uiDevice.findObject(By.text("someText1")))
-    }
-
-    @Test
-    fun activity_CorrectShowRepositoryName() {
-        val editText = uiDevice.findObject(By.res(packageName, "searchEditText"))
-        editText.text = "someText"
-        var toDetails: UiObject2 = uiDevice.wait(
-            Until.findObject(
-                By.res(packageName, "toSearchActivityButton")
-            ), TIMEOUT
-        )
-        toDetails.click()
-        editText.text = ""
-        Assert.assertNotNull(
-            uiDevice.wait(
-                Until.findObject(By.text("SomeText/SomeTXT")),
-                TIMEOUT
-            )
-        )
-        Assert.assertNull(uiDevice.wait(Until.findObject(By.text("someText")), TIMEOUT))
     }
 
     companion object {
